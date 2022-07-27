4a05e33303af5693eed7cc70af9a6f938989fc67
Sasha-PC\Sasha <aleksandr.v.g.m@gmail.com>
2022-07-27 21:48:13 +0300

    solution Task #4. Primitives

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        System.out.println((int)'c');
+        System.out.println((int)'h');
+        System.out.println((int)'a');
+        System.out.println((int)'r');
+
+        System.out.println((char)99);
+        System.out.println((char)104);
+        System.out.println((char)97);
+        System.out.println((char)114);
     }
 }
